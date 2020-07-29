17c87886703a620f6e86a7ec81458109a9116142
Stella Laurenzo <stellaraccident@gmail.com>
2020-07-29 00:34:41 +0000

    Add the manylinux2014-bazel-x64 image

--- scripts/setup/install-dockcross-images.sh
+++ scripts/setup/install-dockcross-images.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Create scripts for dockcross images we care about.
 # Run with sudo.
-DOCKCROSS_IMAGES="manylinux2014-x64"
-
-for img in $DOCKCROSS_IMAGES; do
-  echo "### Setting up dockcross image $img ###"
-  docker run --rm dockcross/$img > /usr/local/bin/dockcross-$img
-  chmod a+x /usr/local/bin/dockcross-$img
+DOCKCROSS_IMAGES="dockcross/manylinux2014-x64 gcr.io/iree-oss/manylinux2014-bazel-x64"
+BIN_DIR="${1:-/usr/local/bin}"
+for url in $DOCKCROSS_IMAGES; do
+  echo "### Setting up dockcross image $url ###"
+  img="$(basename $url)"
+  docker run --rm $url > $BIN_DIR/dockcross-$img
+  chmod a+x $BIN_DIR/dockcross-$img
 done
