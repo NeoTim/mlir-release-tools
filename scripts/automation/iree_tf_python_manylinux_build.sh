85004fbbbc040735de9c426d2bc9299bf0e4860b
Stella Laurenzo <stellaraccident@gmail.com>
2020-07-29 14:55:57 -0700

    Mount /dev/shm with exec.

--- scripts/automation/iree_tf_python_manylinux_build.sh
+++ scripts/automation/iree_tf_python_manylinux_build.sh
@@ -14,8 +14,11 @@ if [ "$1" != "indocker" ]; then
   # rm -Rf build install .doit.db
   # TODO: Bazel only build reliably on large core systems if running from
   # a ram disk. File an issue with the bazel team.
+  # Note that the default /dev/shm is mounted noexec under docker. These
+  # options are needed to avoid the default mount and mount it correctly.
+  DOCKER_ARGS="-v /dev/shm --tmpfs /dev/shm:rw,nosuid,nodev,exec,size=30g --env BAZEL_OUTPUT_BASE=/dev/shm/bazel-out"
   dockcross-manylinux2014-bazel-x64 \
-    --args "--shm-size=30G --env BAZEL_OUTPUT_BASE=/dev/shm/bazel-out" \
+    --args "$DOCKER_ARGS" \
     -- "./$0" indocker
 else
   set -x
