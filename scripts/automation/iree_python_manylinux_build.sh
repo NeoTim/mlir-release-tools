b05d58822e04c79444b08632474ac67a2dae3415
Stella Laurenzo <stellaraccident@gmail.com>
2020-08-04 14:54:07 -0700

    Sync cache in IREE manylinux build.

--- scripts/automation/iree_python_manylinux_build.sh
+++ scripts/automation/iree_python_manylinux_build.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 set -e
 
+MRT_SHARED_CACHE_DIR="${MRT_SHARED_CACHE_DIR:-$HOME/.mrtcache}"
+
 function die() {
   echo "$@"
   exit 1
 }
 [ -f "dodo.py" ] || die "Must be run from the repo root"
 
+function cleanup_outer() {
+  echo "Pushing to cache..."
+  ./scripts/automation/sync_cache.py --push ./cache "$MRT_SHARED_CACHE_DIR"
+}
+
 if [ "$1" != "indocker" ]; then
   set -x
   mkdir -p install
-  # Clean up prior?
-  # rm -Rf build install .doit.db
+  ./scripts/automation/sync_cache.py --pull ./cache "$MRT_SHARED_CACHE_DIR"
+  trap cleanup_outer EXIT
+  trap cleanup_outer ERR
   dockcross-manylinux2014-x64 "./$0" indocker
 else
   set -x
